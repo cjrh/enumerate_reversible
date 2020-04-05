00edadadc5871b1c0a4bc1a3c8cc97a02e7b94a8
Caleb Hattingh <caleb.hattingh@gmail.com>
2020-04-05 23:12:36 +1000

    Added tests

--- enumerate_reversible.py
+++ enumerate_reversible.py
@@ -4,22 +4,12 @@ original_enumerate = enumerate
 def enumerate(iterable, start=0):
     class Inner:
         def __iter__(self):
-            yield from original_enumerate(iterable, start=start)
+            yield from original_enumerate(iterable, start=start or 0)
 
         def __reversed__(self):
+            stt = start or 0
             rev = reversed(iterable)  # First, for accurate exception msg
-            rng = range(len(iterable) - 1 + start, -1 + start, -1)
+            rng = range(len(iterable) - 1 + stt, -1 + stt, -1)
             yield from zip(rng, rev)
 
     return Inner()
-
-
-if __name__ == "__main__":
-    for i, v in enumerate(range(3), start=5):
-        print(i, v)
-
-    for i, v in reversed(enumerate(range(3), start=5)):
-        print(i, v)
-
-    for i, v in reversed(enumerate(iter([1, 2, 3]), start=5)):
-        print(i, v)
---
+++ tests/test_main.py
@@ -0,0 +1,78 @@
+import pytest
+from enumerate_reversible import enumerate as new_enumerate
+
+
+sequences = [
+    # Lists
+    [],
+    [1],
+    [1, 2, 3],
+    ['a'],
+    ['a', 'b', 'c'],
+    # Tuples
+    tuple(),
+    (1, ),
+    (1, 2, 3),
+    # Views
+    dict().keys(),
+    dict().values(),
+    dict().items(),
+    dict(a=1, b=2, c=2).keys(),
+    dict(a=1, b=2, c=2).values(),
+    dict(a=1, b=2, c=2).items(),
+    # Ranges
+    range(0),
+    range(3),
+    range(1000),
+    # Strings
+    '',
+    'a',
+    'abc',
+    # Bytes
+    b'',
+    b'a',
+    b'abc',
+    # Bytearray
+    bytearray(b''),
+    bytearray(b'a'),
+    bytearray(b'abc'),
+    # Memoryview
+    memoryview(b''),
+    memoryview(b'a'),
+    memoryview(b'abc'),
+]
+
+forward_values = [
+    (
+        s, start, list(enumerate(s, start=start))
+    ) for s in sequences for start in range(5)
+]
+
+
+reverse_values = [
+    (
+        s, start, list(reversed(list(enumerate(s, start=start))))
+    ) for s in sequences for start in range(5)
+]
+
+
+@pytest.mark.parametrize('seq,start,result', [
+    *forward_values,
+])
+def test_enumerate_forward(seq, start, result):
+    outputs = []
+    for i, v in new_enumerate(seq, start=start):
+        outputs.append((i, v))
+
+    assert outputs == result
+
+
+@pytest.mark.parametrize('seq,start,result', [
+    *reverse_values,
+])
+def test_enumerate_reverse(seq, start, result):
+    outputs = []
+    for i, v in reversed(new_enumerate(seq, start=start)):
+        outputs.append((i, v))
+
+    assert outputs == result
